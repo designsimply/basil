c79175ac9dc1debf13131423d79a6bc22579c0d4
earthastronaut <>
2020-03-28 22:35:35 -0600

    remove helloworld app

--- web/apps/helloworld/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from helloworld import views
-
-urlpatterns = [
-    path('', views.helloworld),
-]
--- web/apps/helloworld/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def helloworld(request):
-    context = {
-        'var1': 'Variables!',
-    }
-    return render(request, 'helloworld/index.html', context)
