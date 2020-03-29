609b1dc61d8c526523aa8d75cca09a0dff1902a2
earthastronaut <>
2020-03-28 22:54:14 -0600

    removed helloworld urls

--- web/basil/urls.py
+++ web/basil/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', include('helloworld.urls')),
     path('links', include('links.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('.sadmin/', admin.site.urls),
