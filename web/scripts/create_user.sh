002d7eec1bca28830b4c50eabc80f031b9acf193
earthastronaut <>
2020-04-05 14:22:21 -0600

    improve quickstart cmd with create and delete user

--- web/scripts/create_user.sh
+++ web/scripts/create_user.sh
@@ -5,26 +5,33 @@ password=$2
 
 if [[ "$username" == "" || $password == "" ]]
 then
-	echo 'usage: create_user.sh username password [is_staff]'
+	echo 'usage: create_user.sh username password usertype'
 	exit 1
 fi
 
-if [[ "$3" == "is_staff" ]]
+if [[ "$3" == "staff" ]]
 then
 	is_staff='True'
 	is_superuser='False'
-elif [[ "$3" == "is_superuser" ]]
+elif [[ "$3" == "superuser" ]]
 then
 	is_staff='True'
 	is_superuser='True'
-else
+elif [[ "$3" == "regular" ]]
+then
 	is_staff='False'
 	is_superuser='False'
+else
+	echo 'User type must be: regular, staff, superuser'
+
 fi
 
-docker-compose exec web \
-	django-admin shell \
+
+wait_for_database.sh
+
+django-admin shell \
 	--command="from django.contrib.auth.models import User; user=User(username='${username}', is_staff=${is_staff}, is_superuser=${is_superuser}); user.set_password('${password}'); user.save()"
+
 exitcode=$?
 
 if [[ $exitcode == 0 ]]
